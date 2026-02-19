6122bf2c995ea35f94fd3fead371b56c232a9e2e
GooseTeaRice <gusti.fatu@gmail.com>
2026-02-20 09:51:03 +1100

    some changes

--- scripts/dialogueData.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name DialogueData
-
-@export_multiline var text: String
-@export var choices: Array[String] = [] # e.g., ["Yes", "No"]
-@export var next_nodes: Array[DialogueData] = [] # Maps to the choice index
